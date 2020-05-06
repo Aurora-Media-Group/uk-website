803748a3c50fb0451be4a41e8fb3d8aaf1cd7b04
umarf786 <onskadthecamel786@outlook.com>
2020-05-06 18:06:49 +0100

    Updates

---
+++ precache-manifest.abb34bb900f67ae35a3f2a61b524c877.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f866e8e06bbb728880de56034087cdb9",
+    "url": "/uk-website/index.html"
+  },
+  {
+    "revision": "788eb75bcbbd9b2e6e78",
+    "url": "/uk-website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "59b4445ddc2394cf1f10",
+    "url": "/uk-website/static/js/2.44ad3d36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/uk-website/static/js/2.44ad3d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "788eb75bcbbd9b2e6e78",
+    "url": "/uk-website/static/js/main.aefeb5e8.chunk.js"
+  },
+  {
+    "revision": "ce179eec55b56f4a6c20",
+    "url": "/uk-website/static/js/runtime-main.c781ec85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uk-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
