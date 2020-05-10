05a86cc3b1570005b10f95fefb547dd0d8855cbe
umarf786 <onskadthecamel786@outlook.com>
2020-05-10 07:35:45 +0100

    added pic

--- src/About/js/AboutContent.js
+++ src/About/js/AboutContent.js
@@ -5,7 +5,14 @@ export default function AboutContent() {
     <div>
       <div>
         <div className='container'>
-          <div className='parallax'></div>
+          <div className='parallax'>
+            <div className='parentaboutblock'>
+              <div className='aboutblock'>
+                <h3>About Us</h3>
+              </div>
+              <div className='aboutblockcontent'></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
